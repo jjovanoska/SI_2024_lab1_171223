7bf134d564ad782e3473f2e07dd163e9dcaaccc3
Jovana Jovanoska <jovana.jovanoska@yahoo.com>
2024-04-14 17:57:50 +0200

    Fix wrong condition in filterOddNumbers.

--- SILab1.java
+++ SILab1.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SILab1 {
-    public static List<Integer> filterOddNumbers(List<Integer> list) {
+    public static List<Integer> filterOddNumber(List<Integer> list) {
         List<Integer> result = new ArrayList<>();
         for (Integer el : list) {
             if (el % 2 == 0) {
@@ -13,9 +13,9 @@ public class SILab1 {
         return result;
     }
 
-    private static int sumEvenNumbers(List<Integer> list) {
+    private static int sumEvenNumber(List<Integer> list) {
         int sum = 1;
-        for (Integer el : list) {
+        for (Integer el : list){
             if (el % 2 == 0) {
                 sum *= el;
             }
